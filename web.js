6f189cc6da1b7b75e6551b3230ac3ec211ef728d
Alvis Cheung <indreamhk@gmail.com>
2013-06-22 22:57:29 +0800

    Update for cloud9

--- web.js
+++ web.js
@@ -1,31 +1,44 @@
+var http = require('http');
 var express = require('express'),
-    server = express.createServer(),
     pub = __dirname + '/static/',
     views = __dirname + '/views',
     html2jade;
 
 try {
-	html2jade = require('html2jade');
+    html2jade = require('html2jade');
 } catch (err) {
-	console.log('Failure to load \'html2jade\' module');
-	html2jade = require('./node_modules/html2jade/lib/html2jade')
+    console.log('Failure to load \'html2jade\' module');
+    html2jade = require('./node_modules/html2jade/lib/html2jade')
 }
 
-server.use(express.bodyParser());
-server.use(express.static(pub));
-server.set('view engine', 'jade');
-server.set('views', views);
+var app = express();
 
-server.post('/convert', function (req, res) {
-	var html = req.body.html;
+app.configure(function () {
+  app.use(express.compress());
+  app.enable('trust proxy');
+  app.disable('x-powered-by');
+  app.set('views', __dirname + '/views');
+  app.set("view engine", "jade");
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.use(express.static(pub));
+  app.use(express.errorHandler());
+});
+
+app.post('/convert', function (req, res) {
+    var html = req.body.html;
 
     html2jade.convertHtml(html, {}, function (err, jade) {
         res.json({ jade: jade });
     });
 });
 
-server.get('/', function (req, res) {
+app.get('/', function (req, res) {
     res.render('index');
 });
 
-server.listen(process.env.PORT || 9999);
+var ip = process.env.IP;
+var port = process.env.PORT;
+http.createServer(app).listen(port, ip, function () {
+  console.log("Express server listening on port " + ip + ':'+ port + '. Node version: '+ process.version);
+});
